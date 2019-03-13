d29c4d9065fb1550b7d0223d5f79b9cb6ccd2065
bpreki <prescott.byron@gmail.com>
2019-03-13 20:59:55 +0000

    form handling best practice in React

--- reactForms/src/containers/container1.js
+++ reactForms/src/containers/container1.js
@@ -1,37 +1,30 @@
 import React, { Component } from 'react'
 
 class Container1 extends Component {
-    constructor(props){
-        super(props)
 
-        this.state = {
-            stateprop1: "our initial state",
-            stateprop2: 5
-        }
+    state = {
+        value: ''
     }
- 
-    changeState = () => (
-        this.setState({stateprop2: this.state.stateprop2 + 1}
-        ))
-    changeState2 = () => (
-        this.setState({stateprop1: this.state.stateprop1 + "L"}
-        ))
-    multiState = () => (
-        this.setState({stateprop1: this.state.stateprop1 + "A LA",
-                        stateprop2: this.state.stateprop2 + 3}
-                        ))
- 
+
+    handleChange = (event) => (
+        this.setState({value: event.target.value})
+    )
+    
+    handleSubmit = (event) => {
+        event.preventDefault()
+        console.log(this.state.value)
+    }
 
     render() {
-        return (
+        return(
             <div>
-                <button onClick={()=> this.changeState()}> Change State </button>
-                <button onClick={()=> this.changeState2()}> Change State </button>
-                <button onClick={()=> this.multiState()}> MAGIC !! </button>
-                <br /> 
-                state 1: {this.state.stateprop2}
-                <br />
-                state 2: {this.state.stateprop1}
+                React <br />
+                {this.state.value}
+            <form onSubmit={this.handleSubmit}>
+            <label> Name </label>
+            <input id='name' onChange={this.handleChange} type='text' />
+            <button type='submit'> Submit </button>
+            </form>
             </div>
         )
     }
