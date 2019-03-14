b1e4dbea462cdbf8816b4474d2ca61d27e3ea57d
bpreki <prescott.byron@gmail.com>
2019-03-14 16:38:19 +0000

    Redux micro app to show concepts

--- redux/app1/src/App.js
+++ redux/app1/src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
-import './App.css';
+import Container1 from './containers/container1'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <div>
+        React
+        <Container1 />
+      </div>
       
       </div>
     );
